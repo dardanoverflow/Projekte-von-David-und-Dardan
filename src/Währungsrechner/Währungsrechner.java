f531072c56b52d480b4ae01dc987d4f12dcd3de5
david <david@DESKTOP-N7MELF5.Speedport_W_724V_01011603_07_002>
2020-11-11 15:54:59 +0100

    erster Versuch

--- src/Währungsrechner/Währungsrechner.java
+++ src/Währungsrechner/Währungsrechner.java
@@ -1,11 +1,40 @@
 package Währungsrechner;
+import java.util.Scanner;
 
 public class Währungsrechner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		do {
+		
+		System.out.println("Möchten Sie die Umrechung in US-Dollar(1) oder Euro(2) umrechen? ");
+		int i; 
+		i = sc.nextInt();
+		if (i == 1) {
+			System.out.println("Bitte geben sie Ihren Betrag in Euro ein ");
+			double t; 
+			t = sc.nextDouble(); // für die Umrechunug
+			t = t * 1.18;
+			System.out.println("Somit haben sie " + t + " Us-Dollar"); //Ausgabe 
+		} else if (i == 2) {
+			System.out.println("Bitte geben sie Ihren Betrag in Dollar ein ");
+			double t;
+			t = sc.nextDouble();
+			t = t / 1.18; //Aktueller Umrechnungswert
+
+		} else {
+			System.out.println("Bitte geben sie nur eine 1 oder 2 ein "); //Damit die nur eine 1 oder 2 eingeben werden kann.
 		}
-	
+            System.out.println("Möchten Sie nochmal eine umrechung durchführen? J/N ");
+            char J;
+            char N; 
+            i = sc.next();
+		} while ( J );
+			System.out.println("Danke das Sie uns gewählt haben. Schönen Tag ");
+		
+		}
 		
 	}
 
