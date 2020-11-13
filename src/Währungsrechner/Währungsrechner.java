67aeb87a203728eae5648ef8f7584e3b452e1e43
Dardan <Dardan@Chimperial.fritz.box>
2020-11-13 18:40:39 +0100

    Währungsrechner (fertig)

--- src/Währungsrechner/Währungsrechner.java
+++ src/Währungsrechner/Währungsrechner.java
@@ -3,37 +3,52 @@ import java.util.Scanner;
 
 public class Währungsrechner {
 
-	private static final String String = null;
-
-	public static void main(String[] args) {
+	static void Rechner() {
 		Scanner sc = new Scanner(System.in);
-		
+		boolean invalidnum = true;
+		double t;
 		System.out.println("Möchten Sie die Umrechung in US-Dollar(1) oder Euro(2) umrechen?: ");
-		int i; 
-		i = sc.nextInt();
-		if (i == 1) { 		// leitet die Umrechnung von Euro in Dollar ein
+		do {
+		int i = sc.nextInt();
+		if (i == 1) { 																							// leitet die Umrechnung von Euro in Dollar ein
 			System.out.println("Bitte geben sie Ihren Betrag in Euro ein: ");
-			double t; 
+			invalidnum = false;	
 			t = sc.nextDouble();
 			System.out.println("Ihr angegebener Euro Wert entspricht " + t*1.18 + " US-Dollar.");
-		} else if (i == 2) {		//leitet die Umrechnung von Dollar in Euro ein			
+			} else if (i == 2) {
+			invalidnum = false;																				//leitet die Umrechnung von Dollar in Euro ein			
 			System.out.println("Bitte geben sie Ihren Betrag in Dollar ein: ");
-			double t;
-			t = sc.nextDouble();
+			t = sc.nextDouble();				
 			System.out.println("Ihr Betrag entspricht "+ t/1.18+ "Euro.");
-		} else {
-			System.out.println("Bitte geben sie nur eine 1 oder 2 ein: "); //1 oder 2 als einzige Eingabemöglichkeiten
-		}
-            System.out.println("Möchten Sie nochmal eine Umrechung durchführen? J oder N "); //Schleife, wiederholen?
-            String yes = "J";
-            String no = "N";
-            String a = sc.nextLine();
-            if (a == no) {
-            	System.out.print("Die Umrechnung wurde beendet.");
-            }
-            
+			} else {
+			
+			System.out.println("Bitte geben sie nur eine 1 oder 2 ein: ");
+			}
+		} while (invalidnum);
+	}
+
+	public static void main(String[] args) {
 		
-            
-            
+		Scanner sc = new Scanner(System.in);
+		Rechner();
+		boolean loop = true;
+		while(loop) {
+			System.out.print("Möchten Sie noch mal eine Umrechnung durchführen? (J)a oder (N)ein?: ");
+			String answer = " "; answer = sc.nextLine();	
+			if (answer.equals("ja")) {
+				Rechner();
+			}
+			else if (answer.equals("nein")) {
+				loop = false;
+				System.out.println("Sie haben den Währungsrechner beendet.");
+			} else {
+				System.out.println("Bitte wählen Sie nur zwischen ja und nein: ");
+			}
+		}
 	}
 }
+ 
+            	
+            
+            
+            
